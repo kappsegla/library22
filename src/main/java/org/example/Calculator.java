7a426738bfb45b211b335f50ef74118c041683a0
Martin Blomberg <martin.blomberg@outlook.com>
2022-01-21 11:42:30 +0100

    Version 1.1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int mul(int a, int b) {
+        return 4;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -10,4 +10,10 @@ public class CalculatorTest {
     void addReturnsSum() {
         assertEquals(3, Calculator.add(1, 2));
     }
+
+    @Test
+    void mulReturnsProduct() {
+        assertEquals(4, Calculator.mul(2, 2));
+    }
+
 }
